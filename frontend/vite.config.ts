49b25b74966df1c1350d3979d65fc35db235590b
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-02-06 22:40:08 -0600

    Initial commit

---
+++ frontend/vite.config.ts
@@ -0,0 +1,33 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgrPlugin from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": "Enter your backend host with port inside the quotes",
+    },
+  },
+  plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
+  build: {
+    outDir: "./build",
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
+    setupFiles: "./src/test/setup.ts",
+  },
+});
