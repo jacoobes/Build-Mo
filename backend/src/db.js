f435c2b85658c1f051154213676cd082a4ab21d8
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-02-18 23:28:22 -0600

    nmerge

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,7 @@
 import User from '../models/user.js';
 import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017/userdatabase'; // Default MongoDB connection URL
+const url = 'mongodb://127.0.0.1:27017/userdatabase'; // Default MongoDB connection URL
 
 async function connectDB() {
     try {
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import User from '../models/user.js';
+import {connectDB} from './db.js';
 import bcrypt from 'bcryptjs';
 import User from '../models/user.js';
 import { connectDB } from './db.js';
