d36040b724cb3a75d7ce20d1e793217d8cad1f90
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-02-19 01:12:23 -0600

    example shad

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
 // App.js
 
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes component
 import RegisterPage from './Register.jsx';
 import LoginPage from './Login.jsx';
-import { Button } from '@/components/ui/button'
+import AuthenticationPage from './Home.jsx';
 
 const App = () => {
   return (
     <Router>
       <Routes> {/* Wrap Route components in Routes */}
-        <Route path="/" element={<Button>Hola como estas</Button>}/>
+        <Route path="/" element={<AuthenticationPage/>}/>
         <Route path="/register" element={<RegisterPage />} /> {/* Use element prop */}
         <Route path="/login" element={<LoginPage />} /> {/* Use element prop */}
         {/* Add more routes as needed */}
