3ce608e1c9789cd44a8f83b613849314dcfda469
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-02-19 00:49:08 -0600

    jacob: setup shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes component
 import RegisterPage from './Register.jsx';
 import LoginPage from './Login.jsx';
+import { Button } from '@/components/ui/button'
 
 const App = () => {
   return (
     <Router>
       <Routes> {/* Wrap Route components in Routes */}
-        <Route path="/" element={<a>Hola como estas</a>}/>
+        <Route path="/" element={<Button>Hola como estas</Button>}/>
         <Route path="/register" element={<RegisterPage />} /> {/* Use element prop */}
         <Route path="/login" element={<LoginPage />} /> {/* Use element prop */}
         {/* Add more routes as needed */}
