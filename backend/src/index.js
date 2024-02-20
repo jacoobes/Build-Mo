e39286083873c9a0a2fa3a6381d4f1ce92d01bb0
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-02-19 19:59:26 -0600

    add more routes to fetch json from dataset

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import User from '../models/user.js';
 import {connectDB} from './db.js';
 import bcrypt from 'bcryptjs';
 import cors from 'cors';
+import { ls_json, read_json } from './dataset.js';
 
 const app = express();
 
@@ -65,6 +66,39 @@ app.post('/login', async (req, res) => {
         res.status(500).json({ error: 'Failed to authenticate user' });
     }
 });
+
+//Fetching from json dataset
+app.get('/api/json/:name', async (req, res) => {
+    const jsonName = req.params.name;
+    if(!jsonName) {
+        res.status(400).json({ message: "Undefined name" }) 
+        return;
+    }
+    let json_file = null
+    try {
+        json_file = await read_json(jsonName);        
+    } 
+    catch(e) {}
+    if(!json_file) {
+        res.status(400).json({ message: "Could not find json data" }) 
+        return;
+    }
+    res.status(200)
+       .json(json_file)
+})
+app.get('/api/categories', async (_, res) => {
+    let json_file = null
+    try {
+        json_file = await ls_json();        
+    } 
+    catch(e) {}
+    if(!json_file) {
+        res.status(400).json({ message: "Could not find json data" }) 
+        return;
+    }
+    res.status(200)
+       .json(json_file)
+})
 
 const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
