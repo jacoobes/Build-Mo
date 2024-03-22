b4b38b223d75022796098663d76b1f887019be8e
Zachary Pham <elswordmaster15@gmail.com>
2024-03-21 19:14:59 -0500

    I'm down bad for Gojo Satoru

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,51 +1,60 @@
 import User from '../models/user.js';
 import mongoose from 'mongoose';
+import Pcbuild from '../models/pcbuild.js';
 
 const url = 'mongodb://127.0.0.1:27017/userdatabase'; // Default MongoDB connection URL
 
 async function connectDB() {
     try {
-
         await mongoose.connect(url);
         console.log('Connected to MongoDB server');
-
     } catch (err) {
         console.error('Failed to connect to the database:', err);
         process.exit(1);
     }
 }
 
-async function addItem(userId, itemData) {
+async function addBuild(userId, itemData) {
     try {
-        await User.findByIdAndUpdate(userId, { $push: { items: itemData } });
-        return { success: true, message: 'Item added successfully' };
-    } catch (error) {
-        console.error('Error adding item:', error);
+        const newBuild = new Pcbuild({
+            userId: userId,
+            items: [itemData]
+        });
+
+    await newBuild.save();
+
+    await User.findByIdAndUpdate(userId, { $push: { builds: newBuild._id } });
+
+    return { success: true, message: 'Item added successfully' };
+    } catch (err){
+        console.error('Error adding item:', err);
         return { success: false, error: 'Failed to add item' };
     }
 }
 
-async function deleteItem(userId, itemId) {
+async function deleteBuild(userId, buildId) {
     try {
-        await User.findByIdAndUpdate(userId, { $pull: { items: { _id: itemId } } });
-        return { success: true, message: 'Item deleted successfully' };
-    } catch (error) {
-        console.error('Error deleting item:', error);
+    await Pcbuild.findByIdAndDelete(buildId);
+
+    await User.findByIdAndUpdate(userId, { $pull: { builds: buildId } });
+
+    return { success: true, message: 'Item deleted successfully' };
+    } catch (err){
+        console.error('Error deleting item:', err);
         return { success: false, error: 'Failed to delete item' };
     }
 }
 
-async function updateItem(userId, itemId, newItemData) {
+async function updateBuild(userId, buildId, newBuildData) {
     try {
-        await User.findOneAndUpdate(
-            { _id: userId, 'items._id': itemId },
-            { $set: { 'items.$': newItemData } }
-        );
+        await Pcbuild.findByIdAndUpdate(buildId, newBuildData);
+
         return { success: true, message: 'Item updated successfully' };
-    } catch (error) {
-        console.error('Error updating item:', error);
+    } catch (err) {
+        console.error('Error updating item:', err);
         return { success: false, error: 'Failed to update item' };
     }
 }
 
-export { connectDB, updateItem, deleteItem, addItem, url } ;
+
+export { connectDB, updateBuild, deleteBuild, addBuild, url } ;
