20439496a7fc3b06f93a732be6a655da228fb29c
hchoi21802 <126013409+hchoi21802@users.noreply.github.com>
2024-04-04 16:39:37 -0500

    Create BuildTile.jsx
    hi

---
+++ frontend/src/BuildTile.jsx
@@ -0,0 +1,28 @@
+// BuildTile.jsx
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const BuildTile = ({ buildId, title }) => {
+  const navigate = useNavigate();
+
+  const goToBuildDetails = () => {
+    navigate(`/build/${buildId}`);
+  };
+
+  // tailwind styling
+  return (
+    <div
+      onClick={goToBuildDetails}
+      className="w-full h-full bg-primary text-white font-bold rounded-lg hover:bg-primary-hover transition duration-300 cursor-pointer flex flex-col items-center justify-center"
+    >
+      <span className="text-xl mb-2">{title}</span>
+      {/* bio here */}
+      <span className="text-sm">More Info</span>
+      {/* image ere */}
+      <div className="w-16 h-16 bg-gray-200 rounded-full mt-2"></div>
+    </div>
+  );
+};
+
+
+export default BuildTile;
