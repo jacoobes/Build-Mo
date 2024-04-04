ffa2ed3d9b55030eb5aea07d245b3119e4d53dfd
hchoi21802 <126013409+hchoi21802@users.noreply.github.com>
2024-04-04 16:35:59 -0500

    Update Home.jsx

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,41 +1,11 @@
-import { Link } from "react-router-dom";
-import { useState } from 'react'
-import { cn } from "@/lib/utils"
-import { buttonVariants } from "@/components/ui/button"
-import { useLocalStorage } from "./hooks/useLocalStorage";
-
+import React from 'react';
+import './index.css';
+// Other imports as necessary
 
 export default function MainPage() {
   return (
     <div>
-      <nav
-        className="flex justify-between items-center px-4 py-4 text-black bg-white"
-        style={{ fontFamily: 'Poppins, sans-serif' }}>
-        <div className="flex items-center space-x-4">
-          <Link to="/" className="text-2xl font-bold">
-            Build'Mo
-          </Link>
-        </div>
-        <div className="flex items-center justify-between space-x-20 text-xl font-medium">
-          <Link to="/build" className="hover:text-gray-300">
-            Build
-          </Link>
-          <Link to="/parts" className="hover:text-gray-300">
-            Parts
-          </Link>
-          <Link to="/forums" className="hover:text-gray-300">
-            Forums
-          </Link>
-        </div>
-        <div>
-          <Link
-            to="/login"
-            className="bg-primary hover:bg-primary-hover text-white font-bold py-2 px-4 rounded">
-            Login
-          </Link>
-        </div>
-      </nav>
-
+      
       <div className="flex flex-col items-center justify-center h-screen bg-white text-black"
       style={{ fontFamily: 'Poppins, sans-serif' }}>
         <h1 className="text-4xl font-bold mb-4">Empower Your Setup</h1>
@@ -47,3 +17,4 @@ export default function MainPage() {
     </div>
   );
 }
+
